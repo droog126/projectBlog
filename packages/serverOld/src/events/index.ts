475e48f356bf8032d57451d15b79248ab9c16f01
droog126 <n1212126000@gmail.com>
2022-03-08 02:55:47 +0800

    s

--- packages/serverOld/src/events/index.ts
+++
@@ -1,7 +0,0 @@
-const { encode, decode } = require("msgpack5")();
-
-export const Connect = (data: any, socket: any) => {
-  const { path } = data;
-  socket.send(JSON.stringify(encode({ path })));
-  console.log("确认连接成功。。");
-};
